62801969f6fbd68d5c439566afb5400b6cfe3060
Tom Wigington <tom.wigington@gmail.com>
2014-07-21 23:21:46 -0500

    Starting to flush out tests.

---
+++ src/main/java/org/feesh/Life.java
@@ -0,0 +1,7 @@
+package org.feesh;
+
+/**
+ * Created by Thomas on 7/21/2014.
+ */
+public class Life {
+}
---
+++ src/test/java/org/feesh/LifeTest.java
@@ -0,0 +1,19 @@
+package org.feesh;
+
+public class LifeTest {
+    public void underpopulation() {
+
+    }
+
+    public void overpopulation() {
+
+    }
+
+    public void survival() {
+
+    }
+
+    public void resurrection() {
+
+    }
+}
